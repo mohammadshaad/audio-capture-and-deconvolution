27a41b0b9d53702a2944d53f87cd1f72303069cc
Mohammad Shaad <mohammadshaadshaikh@gmail.com>
2023-12-21 13:52:46 +0530

    Add audio capture functionality to the application

---
+++ audio_capture_app.py
@@ -0,0 +1,50 @@
+import tkinter as tk
+import pyaudio
+import wave
+
+class AudioCaptureApp:
+    def __init__(self, master):
+        self.master = master
+        self.master.title("Audio Capture App")
+
+        self.record_button = tk.Button(self.master, text="Record", command=self.start_recording)
+        self.record_button.pack(pady=10)
+
+        self.stop_button = tk.Button(self.master, text="Stop", command=self.stop_recording, state=tk.DISABLED)
+        self.stop_button.pack(pady=10)
+
+    def start_recording(self):
+        self.p = pyaudio.PyAudio()
+        self.stream = self.p.open(format=pyaudio.paInt16,
+                                 channels=1,
+                                 rate=44100,
+                                 input=True,
+                                 frames_per_buffer=1024)
+
+        self.frames = []
+        self.record_button.config(state=tk.DISABLED)
+        self.stop_button.config(state=tk.NORMAL)
+
+    def stop_recording(self):
+        self.stream.stop_stream()
+        self.stream.close()
+        self.p.terminate()
+
+        # Save the recorded audio to a WAV file
+        wf = wave.open("output.wav", "wb")
+        wf.setnchannels(1)
+        wf.setsampwidth(self.p.get_sample_size(pyaudio.paInt16))
+        wf.setframerate(44100)
+        wf.writeframes(b"".join(self.frames))
+        wf.close()
+
+        self.record_button.config(state=tk.NORMAL)
+        self.stop_button.config(state=tk.DISABLED)
+
+def main():
+    root = tk.Tk()
+    app = AudioCaptureApp(root)
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
